4aa09b33b8391013d7c09e638cba6d23faa404f7
Alan Coomes <acoomes33@gmail.com>
2020-12-12 17:24:17 -0600

    added seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Faker::Music::Prince.lyric #=> "It's been 7 hours and 16 days since you took your love away."
+
+# Faker::Music::Prince.song #=> "The Beautiful Ones"
+
+# Faker::Music::Prince.album #=> "Purple Rain"
+
+# Faker::Music::Prince.band #=> "The New Power Generation"
+
+# Faker::Music.genre #=> "Rock"
+
+vibe = ["chill", "uplifting", "nostalgic", "psychedelic", "happy", "sad", "romantic", "marley-esque"]
+
+10.times do 
+
+    Song.create(name: Faker::Music::Prince.song, genre: Faker::Music.genre, lyrics: Faker::Music::Prince.lyric, bpm: rand() * 100, vibe: vibe.sample)
+
+end 
+
