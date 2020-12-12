d764015a531e4a25eb90296e59fd19a5d4c43a0f
Alan Coomes <acoomes33@gmail.com>
2020-12-12 17:34:10 -0600

    added controller index and show

--- backend/app/controllers/albums_controller.rb
+++ backend/app/controllers/albums_controller.rb
@@ -1,2 +1,11 @@
 class AlbumsController < ApplicationController
+    def index
+        albums = Album.all 
+        render json: albums
+    end 
+
+    def show
+        album = Album.find_by_id(params[:id])
+        render json: album
+    end 
 end
