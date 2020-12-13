0546b3d6d546a6fe730e723177f1b0285b87ed1c
Alan Coomes <acoomes33@gmail.com>
2020-12-12 19:25:40 -0600

    added redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'redux';
+import { createStore, applyMiddleware } from 'react-redux';
+import { thunk } from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleWare(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider stor={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
