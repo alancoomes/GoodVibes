477e23199120852eb1ac28420826869227bf03e1
Alan Coomes <acoomes33@gmail.com>
2020-12-14 15:44:08 -0600

    added song update and destroy controllers

--- backend/app/controllers/songs_controller.rb
+++ backend/app/controllers/songs_controller.rb
@@ -1,11 +1,13 @@
 class SongsController < ApplicationController
+
+    before_action :set_song, only: [:show, :update, :destroy]
+
     def index
         songs = Song.all 
         render json: songs
     end 
 
     def show
-        song = Song.find_by_id(params[:id])
         render json: song
     end 
 
@@ -14,10 +16,28 @@ class SongsController < ApplicationController
 
         if song.save 
             render json: song
+        else 
+            render song.errors.full_messages, status: :unprocessable_entity
         end 
     end 
 
+    def update
+        if song.update(song_params) 
+            render json: song 
+        else 
+            render song.errors.full_messages, status: :unprocessable_entity
+        end 
+    end 
+
+    def destroy
+        song.destroy 
+    end 
+
     private
+
+    def set_song
+        song = Song.find_by_id(params[:id])
+    end 
 
     def song_params
         params.require(:song).permit(
