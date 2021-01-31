8f4430edc18ec57c238333f2d5e33a52348593f5
Alan Coomes <acoomes33@gmail.com>
2021-01-30 22:33:47 -0600

    added user id to songs and albums

--- backend/app/models/song.rb
+++ backend/app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ApplicationRecord
     belongs_to :album, optional: true
+    belongs_to :user
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_202017) do
+ActiveRecord::Schema.define(version: 2021_01_31_043035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_01_23_202017) do
     t.string "cover_art"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_01_23_202017) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "album_id"
+    t.integer "user_id"
     t.index ["album_id"], name: "index_songs_on_album_id"
   end
 
