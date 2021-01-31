086e124df773493bba9f00b3083c08f02f246a4d
Alan Coomes <acoomes33@gmail.com>
2021-01-30 22:10:40 -0600

    migrated user model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_010001) do
+ActiveRecord::Schema.define(version: 2021_01_23_202017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2020_12_15_010001) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "album_id"
     t.index ["album_id"], name: "index_songs_on_album_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "songs", "albums"
