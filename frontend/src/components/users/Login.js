061ad2371e98946e40d86651a83deafe77e90462
Alan Coomes <acoomes33@gmail.com>
2021-02-06 22:15:04 -0600

    added redux to login

--- frontend/src/components/users/Login.js
+++ frontend/src/components/users/Login.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
 
-export default function Login() {
+const Login = ({ username, password }) => {
   return (
     <form onSubmit={}>
-      <input type="text" onChange={} name="username" value={}></input>
-      <input type="text" onChange={} name="password" value={}></input>
+      <input
+        placeholder="username"
+        type="text"
+        onChange={}
+        name="username"
+        value={username}
+      ></input>
+      <input
+        placeholder="password"
+        type="text"
+        onChange={}
+        name="password"
+        value={password}
+      ></input>
       <input type="submit" value="Login" />
     </form>
   );
-}
+};
+
+const mapStateToProps = (state) => {
+  return {
+    username: state.loginForm.username,
+    password: state.loginForm.password,
+  };
+};
+
+export default connect(mapStateToProps)(Login);
