624e834cae0534edf93896fba14ca82dd0706fef
Alan Coomes <acoomes33@gmail.com>
2021-02-06 18:49:26 -0600

    added user reducer

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -1,1 +1,19 @@
-export default (state = [], action) => 
+function userReducer(state = { all: [] }, action) {
+  switch (action.type) {
+    case "FETCH_USERS_SUCCESS":
+      return { ...state, all: action.payload };
+
+    case "CREATE_USER_SUCCESS":
+      return { ...state, all: state.all.concat(action.payload) };
+
+    case "DELETE_USER_SUCCESS":
+      return {
+        ...state,
+        all: state.all.filter((user) => user.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
