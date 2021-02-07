68229362549387a0c17576dcf2773ba2fe5a8626
Alan Coomes <acoomes33@gmail.com>
2021-02-06 19:21:26 -0600

    added sessions controller

--- frontend/src/redux/actions/currentUser.js
+++ frontend/src/redux/actions/currentUser.js
@@ -7,10 +7,20 @@ export const setCurrentUser = (user) => {
 };
 
 //async
-export const login = (credentials) => {
+export const login = (cred) => {
   return (dispatch) => {
-    fetch("http://localhost:3000/api/v1/users")
-      .then((res) => res.json())
-      .then((user) => dispatch({ type: "LOGIN_SUCCESS", payload: user }));
+    fetch("http://localhost:3000/api/v1/login"),
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: cred.username,
+          password: cred.password,
+        }),
+      }
+        .then((res) => res.json())
+        .then((user) => dispatch({ type: "LOGIN_SUCCESS", payload: user }));
   };
 };
