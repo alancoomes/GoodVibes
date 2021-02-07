5c2ea85d42402778dcb8efbcc635ee77dcd46f2d
Alan Coomes <acoomes33@gmail.com>
2021-02-06 18:22:40 -0600

    added user redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import songReducer from "./reducers/songReducer";
 import albumReducer from "./reducers/albumReducer";
+import userReducer from "./reducers/userReducer";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
@@ -10,6 +11,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
   songs: songReducer,
   albums: albumReducer,
+  users: userReducer,
 });
 
 const persistConfig = {
