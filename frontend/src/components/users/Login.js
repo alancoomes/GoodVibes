cb1638f2e355fd7fdf0391f3426ec4a1c00df8c8
Alan Coomes <acoomes33@gmail.com>
2021-02-06 21:15:23 -0600

    added form to login

--- frontend/src/components/users/Login.js
+++ frontend/src/components/users/Login.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Login() {
-  return <div></div>;
+  return (
+    <form onSubmit={}>
+      <input type="text" onChange={} name="username" value={}></input>
+      <input type="text" onChange={} name="password" value={}></input>
+      <input type="submit" value="Login" />
+    </form>
+  );
 }
